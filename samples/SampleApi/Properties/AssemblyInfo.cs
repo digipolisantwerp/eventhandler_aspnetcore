3f0d41031b197d73fcab0901157d32ddebd4ca6f
Kevin Bosteels <Kevin.Bosteels@Digipolis.be>
2016-07-08 15:17:22 +0200

    Removed old application

--- samples/SampleApi/Properties/AssemblyInfo.cs
+++
@@ -1,2 +0,0 @@
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("SampleApi.UnitTests")]
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("SampleApi.IntegrationTests")]
--- src/Digipolis.Toolbox.Eventhandler/EventHandler/IEventHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Digipolis.Toolbox.Eventhandler
-{
-    public interface IEventHandler
-    {
-        void Publish<T>(string messagetopic, string eventType, T eventContent, string componentId, string componentName, string eventFormat = null);
-        void PublishJson(string messagetopic, string eventType, string eventContent, string componentId, string componentName, string eventFormat = null);
-        void PublishString(string messagetopic, string eventType, string eventContent, string componentId, string componentName, string eventFormat = null);
-    }
-}
